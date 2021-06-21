3b34adcd6a43f8c50af5b0750b08b82c9edd6078
mohora11 <mohora@naver.com>
2021-06-21 17:31:46 +0900

    2021.06.21

---
+++ src/main/java/org/zerock/mapper/BoardMapper.java
@@ -0,0 +1,35 @@
+package org.zerock.mapper;
+
+import java.util.List;
+
+import org.apache.ibatis.annotations.Select;
+import org.zerock.domain.BoardVO;
+
+public interface BoardMapper {
+	
+//	@Select("SELECT * FROM tbl_board")
+	public List<BoardVO> getList();
+	
+	
+	// INSERT INTO ble_board (title, content, writer)
+	// VALUES (#{title}, #{content}, #{writer}) mybatis에서 사용하는 쿼리
+	public int insert(BoardVO board);
+	/*
+	  	String sql = "INSERT INTO tbl_board (title, content, writer) VALUES (?, ?, ?) ";
+	  	...
+	  	pstmt.setString(1, board.getTitle());
+	  	pstmt.setString(2, board.getContent());
+	  	pstmt.setString(3, board.getWriter());
+	  	
+	  	pstmt.executeUpdate();
+	  
+	 */
+	
+	public int insertSelectKey(BoardVO board);
+	
+	public BoardVO read(long bno); //하나만 뽑아서 보는것
+	
+	public int delete(long bno);
+	
+	public int update(BoardVO board);
+}
---
+++ src/test/java/org/zerock/mapper/Test1Tests.java
@@ -0,0 +1,103 @@
+package org.zerock.mapper;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertNull;
+import static org.junit.Assert.assertTrue;
+
+import java.util.List;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.zerock.domain.Test1VO;
+
+import lombok.Setter;
+import lombok.extern.log4j.Log4j;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration("file:src/main/webapp/WEB-INF/spring/root-context.xml")
+@Log4j
+public class Test1Tests {
+	
+	private Test1Mapper mapper;
+	
+	@Autowired
+	public void setMapper(Test1Mapper mapper) {
+		this.mapper = mapper;
+	}
+	
+	@Test
+	public void testMapper() {
+		assertNotNull(mapper);
+	}
+
+	@Test
+	public void testGetList() {
+		List<Test1VO> list = mapper.getList();
+
+		assertTrue(list.size() > 0);
+	}
+
+	@Test
+	public void testInsert() {
+		Test1VO vo = new Test1VO();
+		vo.setName("seoul");
+		vo.setAge(999);
+		
+		int before = mapper.getList().size();
+		
+		mapper.insert(vo);
+		
+		int after = mapper.getList().size();
+		
+		assertEquals(before + 1, after);
+	}
+
+	@Test
+	public void testInsertSelectKey() {
+		Test1VO vo = new Test1VO();
+		vo.setName("jeju");
+		vo.setAge(777);
+
+		mapper.insertSelectKey(vo);
+
+		assertNotEquals(0, vo.getId());
+	}
+
+	@Test
+	public void testRead() {
+		Test1VO vo = mapper.read(3);
+		
+		assertEquals("비글", vo.getName());
+		assertEquals(5, vo.getAge());
+	}
+	
+	@Test
+	public void testDelete() {
+		mapper.delete(2);
+		
+		Test1VO vo = mapper.read(2);
+		
+		assertNull(vo);
+	}
+	
+	@Test
+	public void testUpdate() {
+		
+		Test1VO vo = new Test1VO();
+		vo.setId(3);
+		vo.setName("비글");
+		vo.setAge(5);	
+		
+		mapper.update(vo);
+		
+		Test1VO vo3 = mapper.read(3);
+		assertEquals("비글", vo3.getName());
+		assertEquals(5, vo3.getAge());
+	}
+
+}
