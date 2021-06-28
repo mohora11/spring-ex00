9ed98fdb63b4d673c76e8d5264a59c1283956c71
mohora11 <mohora@naver.com>
2021-06-29 08:58:39 +0900

    2021.06.28

--- src/main/java/org/zerock/controller/lecture/rest/Ex06RestController.java
+++ src/main/java/org/zerock/controller/lecture/rest/Ex06RestController.java
@@ -1,8 +1,15 @@
 package org.zerock.controller.lecture.rest;
 
+import javax.servlet.http.HttpServletRequest;
+
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import lombok.extern.log4j.Log4j;
@@ -14,7 +21,7 @@ public class Ex06RestController {
 	
 	@RequestMapping("/sub01")
 	public String method01() {
-		log.info("rest06 sub01 method01....");
+		log.info("rest06 sub01 method...");
 		return "hello";
 	}
 	
@@ -23,9 +30,37 @@ public class Ex06RestController {
 		return "method02 response";
 	}
 	
+	
 	@RequestMapping("/sub04")
 	public ResponseEntity<String> method04() {
 		
 		return new ResponseEntity<String>(HttpStatus.BAD_REQUEST);
 	}
+	
+	
+	@RequestMapping("/sub05")
+//	@GetMapping
+//	@PostMapping
+//	@DeleteMapping
+//	@PutMapping
+	public String method05(HttpServletRequest request) {
+		
+		log.info(request.getMethod());
+		
+		return "hello";
+	}
+	
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
