5d35273424386663dbd9ef5b9757864cd345101b
mohora11 <mohora@naver.com>
2021-06-30 18:06:07 +0900

    2021.06.30

--- src/main/webapp/WEB-INF/sql/SQL04.sql
+++ src/main/webapp/WEB-INF/sql/SQL04.sql
@@ -12,4 +12,19 @@ CREATE TABLE tbl_reply (
 	FOREIGN KEY (bno) REFERENCES tbl_board(bno)
 );
 
+Select * FROM tbl_board
+ORDER BY bno desc;
+
+SELECT * FROM tbl_reply
+ORDER BY rno DESC;
+
+UPDATE tbl_reply
+SET reply = "수정된 댓글"
+WHERE rno = 4;
+
+-- 댓글이 잇는 게시물 조회
+SELECT
+ DISTINCT (b.bno)
+FROM tbl_board b JOIN tbl_reply r ON b.bno = r. bno;
+
 
