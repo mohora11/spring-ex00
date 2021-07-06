c221682ff9be4154ce725fdbd59c6c70883b414c
mohora11 <mohora@naver.com>
2021-07-06 18:36:58 +0900

    2021.07.06

---
+++ src/main/webapp/WEB-INF/sql/SQL07.sql
@@ -0,0 +1,18 @@
+use spr1;
+
+desc tbl_board;
+
+CREATE TABLE tbl_board_file (
+	id INT PRIMARY KEY AUTO_INCREMENT,
+    bno INT REFERENCES tbl_board(bno),
+    fileName VARCHAR(200) NOT NULL
+);
+
+DELETE FROM tbl_board_file;
+
+ALTER TABLE tbl_board_file
+ADD FOREIGN KEY (bno) REFERENCES tbl_board(bno);
+
+DESC tbl_board_file;
+
+SELECT * FROM tbl_board_file;
